3eeed7d1264dae0f675fbe2343d1429a0dd5ca35
jost <johan.stettler@gmail.com>
2025-02-19 09:15:58 +0100

    bugfiy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/your-repo-name/", // Replace with your actual repo name
+  base: "/spirit_islant_companion_webapp/", 
 });
